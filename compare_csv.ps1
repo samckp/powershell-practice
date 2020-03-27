3d2b0ea40313c29e4e34d46563382d28bb253654
samckp <sam.mca.liet@gmail.com>
2020-03-27 23:06:40 +0530

    Compare two csv files

---
+++ compare_csv.ps1
@@ -0,0 +1,18 @@
+# Path of the 2 CSVs you want to compare
+$csv1 = Import-Csv -Path F:\github\Powershell\powershell-practice\server.csv -delimiter ","  
+$csv2 = Import-Csv -Path F:\github\Powershell\powershell-practice\server1.csv -delimiter "," 
+
+$end     = $csv1.Count
+$count   = 0
+$diffobj = @()
+    
+  do{
+    
+    if(!($csv1[$count].id -eq $csv2[$count].id)){
+       $diffobj += $csv1[$count]
+    } 
+		 
+  $count++
+  }until($count -eq $end)
+
+  $diffobj | export-csv F:\github\Powershell\powershell-practice\out.csv -NoTypeInformation
